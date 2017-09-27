97a6f18d79f4d27f0cd727bb17238960f0180d6a
arizona2014 <andy.lisac@gmail.com>
2017-09-27 21:31:08 +0200

    Added TESTS

--- javascript1.js
+++ javascript1.js
@@ -1,12 +1,21 @@
+Solution: 
+
 function dontGiveMeFive(start, end){
   var count = 0;
   for(i=start; i<=end; i++){
-    if(i<=9){
-      if(i % 5 !== 0) count++;    
-    } else {
-      if(i % 5 !== 0)  count++;    
-      if(i % 10 === 0) count++;
+    if (!/5/.test(i)) {
+      count++
     }
   }
   return count;
 }
+
+TESTS:
+
+Test Results:
+ KataTests
+ exampleTests (2 of 2 Assertions)
+ moreTests (5 of 5 Assertions)
+ randomTests (30 of 30 Assertions)
+Completed in 13ms
+You have passed all of the tests! :)
